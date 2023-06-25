cdf6707c68ed0d1baa035de33f2b3a67c4e8f68c
jeroen <jeroen.smissaert@gmail.com>
2023-06-25 14:09:24 +0200

    Start Project

---
+++ src/test/java/be/common/xrequest/XrequestApplicationTests.java
@@ -0,0 +1,13 @@
+package be.common.xrequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XrequestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
