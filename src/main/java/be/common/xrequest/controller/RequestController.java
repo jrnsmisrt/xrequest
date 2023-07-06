1238929544b91490b0ad780275754fb8ddd5ac86
jeroen <jeroen.smissaert@gmail.com>
2023-07-06 09:07:13 +0200

    Add endpoints to requestcontroller, update requestservice

---
+++ src/main/java/be/common/xrequest/controller/RequestController.java
@@ -0,0 +1,44 @@
+package be.common.xrequest.controller;
+
+import be.common.xrequest.domain.request.dto.RequestDto;
+import be.common.xrequest.service.RequestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/requests")
+public class RequestController {
+    RequestService requestService;
+
+    @Autowired
+    public RequestController(RequestService requestService) {
+        this.requestService = requestService;
+    }
+
+    @GetMapping(produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    public List<RequestDto> getAllRequests() {
+        return requestService.getAllRequests();
+    }
+
+    @GetMapping(produces = "application/json")
+    @ResponseBody
+    public RequestDto getRequestById(@RequestParam String id) {
+        return requestService.getRequestById(id);
+    }
+
+    @PostMapping(produces = "application/json", consumes = "application/json", path = "/new")
+    @ResponseStatus(HttpStatus.CREATED)
+    public RequestDto createRequest(@RequestBody RequestDto requestDto) {
+        return requestService.createRequest(requestDto);
+    }
+
+    @PutMapping(produces = "application/json", consumes = "application/json")
+    @ResponseBody
+    public RequestDto editRequestById(@RequestParam String id, @RequestBody RequestDto requestDto) {
+        return requestService.updateRequestById(id, requestDto);
+    }
+}
--- src/main/java/be/common/xrequest/service/RequestService.java
+++ src/main/java/be/common/xrequest/service/RequestService.java
@@ -1,15 +1,71 @@
 package be.common.xrequest.service;
 
+import be.common.xrequest.domain.request.Request;
+import be.common.xrequest.domain.request.dto.RequestDto;
 import be.common.xrequest.mapper.RequestMapper;
 import be.common.xrequest.repository.RequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 public class RequestService {
+    RequestRepository requestRepository;
+    RequestMapper mapper;
 
-@Autowired
-    public RequestService(RequestRepository repo, RequestMapper mapper){
+    @Autowired
+    public RequestService(RequestRepository requestRepository, RequestMapper mapper) {
+        this.requestRepository = requestRepository;
+        this.mapper = mapper;
+    }
 
-}
+    public RequestDto createRequest(RequestDto requestDto) {
+        Request req = mapper.mapRequestDtoToRequest(requestDto);
+
+        requestRepository.saveAndFlush(req);
+
+        return this.getRequestById(req.getId().toString());
+    }
+
+
+    public List<RequestDto> getAllRequests() {
+        List<RequestDto> dtoList = new ArrayList<>();
+
+        requestRepository.findAll().forEach(r -> {
+            RequestDto mapped = mapper.mapRequestToRequestDto(r);
+            if (!dtoList.contains(mapped)) {
+                dtoList.add(mapped);
+                System.out.println(mapped.getTitle() + " : has been added to dto list");
+            }
+        });
+
+        return dtoList;
+    }
+
+    public RequestDto getRequestById(String id) {
+//        UUID mappedId = UUID.fromString(id);
+//        requestRepository.findAllById(Collections.singleton(mappedId)).stream().findFirst().get();
+        Optional<Request> optional = requestRepository.findAll().stream().filter(req -> req.getTitle().equals(id)).findFirst();
+
+        if (optional.isEmpty()) {
+            throw new NullPointerException();
+        }
+
+        return mapper.mapRequestToRequestDto(optional.get());
+    }
+
+    public RequestDto updateRequestById(String id, RequestDto requestDto) {
+        if (this.getRequestById(id).getId() != null &&
+                (!this.getRequestById(id).getId().isEmpty()
+                        || !this.getRequestById(id).getId().isBlank())) {
+            Request mapped = mapper.mapRequestDtoToRequest(requestDto);
+            requestRepository.saveAndFlush(mapped);
+
+            return this.getRequestById(mapped.getId().toString());
+        } else throw new NullPointerException();
+    }
 }
