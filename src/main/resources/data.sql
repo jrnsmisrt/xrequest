cb07f04b5fa97b0bafeccff1035452500104558d
jrnsmisrt <jeroen.smissaert@gmail.com>
2023-07-29 08:53:08 +0200

    fix h2 issues

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,18 @@
-drop table if exists author;
-drop table if exists xrequest;
-drop table if exists place;
+drop table if exists author cascade;
+drop table if exists xrequest cascade;
+drop table if exists place cascade;
 
+create table place
+(
+    id      integer      not null,
+    city    varchar(200) not null,
+    state   varchar(200),
+    country varchar(200) not null,
+    constraint place_pk primary key (id)
+);
+
+insert into place(id, city, state, country)
+values (1, 'Oostende', 'West-Vlaanderen', 'België');
 
 create table author
 (
@@ -31,17 +42,8 @@ create table xrequest
     CONSTRAINT author_id_fk FOREIGN KEY (author_id) REFERENCES author (id)
 );
 
-create table place
-(
-    id      integer      not null,
-    city    varchar(200) not null,
-    state   varchar(200),
-    country varchar(200) not null,
-    constraint place_pk primary key (id)
-);
-
-insert into author(NAME, NICKNAME, SURNAME, EMAIL, PHONENUMBER, AGE, PLACE)
-values (
+insert into author(ID, NAME, NICKNAME, SURNAME, EMAIL, PHONENUMBER, AGE, PLACE)
+values ('64686c7c-de43-4667-8a13-eb192c0bd080',
         'test user name',
         'test_request_user',
         'surname_testuser',
@@ -50,11 +52,12 @@ values (
         30,
         1);
 
--- insert into xrequest(, AUTHOR_ID, DATETIME, CATEGORY, TITLE, CONTENT, PHOTOS_URL, TAGS)
--- values ('a90efda9-e231-400b-a24f-0365712b2df0',
---         'date time',
---         'COUPLE4COUPLE',
---         'test_title',
---         'test_content',
---         array ['test/url', 'test/url2'],
---         array ['sex, date', 'love', 'fuck']);
+insert into xrequest(id, AUTHOR_ID, DATETIME, CATEGORY, TITLE, CONTENT, PHOTOS_URL, TAGS)
+values ('a90efda9-e231-400b-a24f-0365712b2df0',
+        '64686c7c-de43-4667-8a13-eb192c0bd080',
+        '2023-07-29 08:45:10',
+        'COUPLE4COUPLE',
+        'test_title',
+        'test_content',
+        array ['test/url', 'test/url2'],
+        array ['sex, date', 'love', 'fuck']);
